6a16168852534a8ec855fd3f653a0eda40291f86
SreySitharo <sitharosrey@gmail.com>
2024-08-20 09:57:42 +0700

    add comments to specific about lcel

--- test_lcel/test_chain_operation.py
+++ test_lcel/test_chain_operation.py
@@ -9,6 +9,8 @@ def minus_one(x):
 add_two = RunnableLambda(add_two)
 minus_one = RunnableLambda(minus_one)
 
+# this is a chain that we combine the 2 method to work together
+# lcel is specifically designed to work with the Runnable interface, that why we use RunnableLambda
 chain = add_two | minus_one
 result = chain.invoke(3)
 
