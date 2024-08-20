66feb92823902f63be70c7c46a8a4b2efaea7ed4
SreySitharo <sitharosrey@gmail.com>
2024-08-20 16:10:39 +0700

    test with QuerySQLDataBaseTool to execute the query that get from model

--- test_wtih_db/test_db_with_chain_using_build_in.py
+++ test_wtih_db/test_db_with_chain_using_build_in.py
@@ -1,6 +1,8 @@
 from langchain_ollama.llms import OllamaLLM
 from langchain_community.utilities import SQLDatabase
 from langchain.chains import create_sql_query_chain
+from langchain_community.tools.sql_database.tool import QuerySQLDataBaseTool
+
 
 # Setup db and connect db
 db = SQLDatabase.from_uri(
@@ -9,19 +11,16 @@ db = SQLDatabase.from_uri(
 
 model = OllamaLLM(model="llama3.1:8b-instruct-q5_0")
 
-chain = create_sql_query_chain(model, db)
 
 # we can use this to get prompt from create_sql_query_chain
 # https://python.langchain.com/v0.2/docs/how_to/sql_prompting/
-print("this is prompt : ", chain.get_prompts()[0].pretty_print())
+# print("this is prompt : ", chain.get_prompts()[0].pretty_print())
+
+execute_query = QuerySQLDataBaseTool(db=db)
+write_query = create_sql_query_chain(model, db)
+
+chain = write_query | execute_query
 
 print("loading ...")
-response = chain.invoke({"question": "List class that have the most number of students"})
+response = chain.invoke({"question": "How many students learn Mathematics and Physics"})
 print("response : ", response)
-
-# we can handle exception here, if we want
-if response is not None:
-    result = (db.run(response))
-    print(result)
-else:
-    print("Cannot generate the query, please change modify your question.")
--- test_wtih_db/test_db_with_chain_using_custom_method.py
+++ test_wtih_db/test_db_with_chain_using_custom_method.py
@@ -2,6 +2,7 @@ from langchain_ollama.llms import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_community.utilities import SQLDatabase
+from langchain_community.tools.sql_database.tool import QuerySQLDataBaseTool
 
 # Setup db and connect db
 db = SQLDatabase.from_uri(
@@ -16,12 +17,15 @@ prompt = ChatPromptTemplate.from_template(template)
 
 model = OllamaLLM(model="llama3.1:8b-instruct-q5_0")
 
-chain = prompt | model | StrOutputParser()
+# we can use this class to execute the query
+execute_query = QuerySQLDataBaseTool(db=db)
+
+chain = prompt | model | StrOutputParser() | execute_query
 
 # not handle yet, when model cannot provide the response
 print("loading ... ")
-result = chain.invoke({"dialect": db.dialect,
-                       "schema_info": db.get_table_info(),
+result = chain.invoke({"dialect": db.dialect,  # get the dialect of database
+                       "schema_info": db.get_table_info(),  # get the schema
                        "question": "List top 3 students base on their score"})
 
-print("result : ", db.run(result))
+print("result : ", result)
