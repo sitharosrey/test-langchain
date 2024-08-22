6db2f2508330570bae0fdd97278ef6e4e392cd64
SreySitharo <sitharosrey@gmail.com>
2024-08-23 03:06:31 +0700

    update the parameters

--- test_wtih_db/test_validate.py
+++ test_wtih_db/test_validate.py
@@ -19,17 +19,19 @@ db = SQLDatabase.from_uri("postgresql+psycopg2://postgres:123@localhost:5432/vir
 
 model = ChatGroq(groq_api_key=GROQ_API_KEY, model="llama3-70b-8192")
 
+my_prompt_template = """You are a SQL expert of {dialect}. 
+   Please write an SQL query base on this question: "{question}" and pay attention to use only the column names you can see in the tables below.
+   Be careful to not query for columns that do not exist. Also, pay attention to which column is in which table and if you use the SELECT with GROUP BY, please check it, because if it not appear in GROUP BY it will got error.
 
-def generate_query_and_test_query(llm_model, question, database, check_query=False):
-    prompt_template = """You are a SQL expert of {dialect}. 
-       Please write an SQL query base on this question: "{question}" and pay attention to use only the column names you can see in the tables below.
-       Be careful to not query for columns that do not exist. Also, pay attention to which column is in which table and if you use the SELECT with GROUP BY, please check it because if it not appear in group by it will got error.
+   Here is database information: {schema_info}
 
-       Here is database information: {schema_info}
+   Only provide the SQL script without any explanation and follow this format for the output: 
+   SQL Script: <<Output here>>
+   """
 
-       Only provide the SQL script without any explanation and follow this format for output: 
-       SQL Script: <<Output here>>
-       """
+
+def generate_query_and_test_query(llm_model, prompt_template, question, database, retry, check_query=False):
+    count = 0
 
     input_data = {
         "dialect": database.dialect,
@@ -38,17 +40,18 @@ def generate_query_and_test_query(llm_model, question, database, check_query=Fal
     }
 
     prompt = ChatPromptTemplate.from_template(prompt_template)
-    count = 0
+    chain = prompt | llm_model | StrOutputParser()
 
     while True:
-        print("Hi")
-        chain = prompt | llm_model | StrOutputParser()
+        print("this is count : ", count)
+        count += 1
 
         response_query = chain.invoke(input_data)
-        if response_query or count > 3:
+        if response_query or count > retry:
             # execute the query to database, and if error retry again
-            if response_query and check_query is False:
-                    print("this is in the try catch")
+            if response_query:
+                if check_query is False:
+                    print("this is try to generate the QUERY")
                     final_query = response_query.strip().replace("SQL Script: ", "")
                     print("this is final_query : ", final_query)
 
@@ -56,12 +59,14 @@ def generate_query_and_test_query(llm_model, question, database, check_query=Fal
                         data = database.run(final_query.strip())
                         print("data : ", data)
                     except Exception as e:
-                        # I want to implement the memory here, base on the that regenerate again
+                        # in this section, the model will go try again, but not more than 3 times
                         print("Cannot execute query and here is the error : ", e)
                         continue
+                else:
+                    # i will implement here later
+                    print("this is try to validate the QUERY")
             break
-        count += 1
     return response_query
 
 
-print(generate_query_and_test_query(model, "List all products in tharo shop", db))
+print(generate_query_and_test_query(model, my_prompt_template, "List all products in tharo shop", db, 3))
